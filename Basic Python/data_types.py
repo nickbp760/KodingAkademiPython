359a4280af8bf2cb0784d65c715ca9f4aeea1d03
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-07-07 14:04:09 +0700

    Initial commit

---
+++ Basic Python/data_types.py
@@ -0,0 +1,14 @@
+a = 45
+b = 3.14
+c = 'K'
+d = 'Kevin'
+e = True
+
+convert_float = float(a)
+print(convert_float, type(convert_float))
+convert_int = int(b)
+print(convert_int, type(convert_int))
+convert_hex = hex(a)
+print(convert_hex, type(convert_hex))
+convert_string = str(a)
+print(convert_string, type(convert_string))
