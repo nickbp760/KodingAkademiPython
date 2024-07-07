8073aeaff1018b002f579fcf8fcef7b3c7ca3cad
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-07-07 14:20:41 +0700

    operator

---
+++ Basic Python/operator.py
@@ -0,0 +1,21 @@
+angka = 0
+angka = angka + 2
+angka += 3
+print(angka)
+angka -= 1
+print(angka)
+angka *= 3
+print(angka)
+angka /= 2
+print(angka)
+angka **= 2
+print(angka)
+angka %= 7
+print(angka)
+
+num1 = int(input("Masukkan angka pertama : "))
+num2 = int(input("Masukkan angka kedua : "))
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 == num2)
+print(num1 != num2)
