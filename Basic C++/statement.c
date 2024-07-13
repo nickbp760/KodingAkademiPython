ded7f04f3fa284d0743d0fb196507d54cdde8ebf
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-07-13 16:33:08 +0700

    ok

---
+++ Basic C++/statement.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hallo, tanpa new line");
+    printf("Hallo, dengan new line\n");
+    printf("A string with \"quoted text\" inside of it\n\n");
+    printf("Tabbed\tColumn");
+}
---
+++ Basic C++/var.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+
+    int azmi_ITS;
+    int b = -1;
+    int c = 32768;
+    char d = 'A';
+    float e = 3.14;
+    char yy[] = "Hello World";
+    int abc = 123;
+    abc = 1234;
+    const double PI = 3.14;
+    // PI = 2,9
+    printf("%f \?" ,e);
+    printf("aaaaaa %d %c %c c llllll\n", c, b, d);
+    printf("123 %d\n", abc);
+    printf("%s ", yy);
+    printf("%f ",PI);
+    return 0;
+}
