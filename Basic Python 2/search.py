8e1e2897b961f15a2c3185f2861f2102470844fb
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-07-20 09:16:34 +0700

    ok

--- Basic Python 2/search.py
+++ Basic Python 2/search.py
@@ -7,14 +7,17 @@ with open(input_file, 'r') as file:
     text = file.read()
 
 # Hitung jumlah kata "Gunung"
-count_gunung = text.count('Gunung')
+count_Gunung = text.count('Gunung')
+count_gunung = text.count('gunung')
 
 # Ganti kata "Gunung" dengan "pegunungan"
 modified_text = text.replace('Gunung', 'pegunungan')
+modified_text = text.replace('gunung', 'pegunungan')
 
 # Tulis hasil yang telah dimodifikasi ke file teks baru
 with open(output_file, 'w') as file:
     file.write(modified_text)
 
 # Tampilkan jumlah kata "Gunung" yang terhitung
-print(f'Jumlah kata "Gunung" yang terhitung: {count_gunung}')
+print(f'Jumlah kata "Gunung" yang terhitung: {count_Gunung}')
+print(f'Jumlah kata "gunung" yang terhitung: {count_gunung}')
