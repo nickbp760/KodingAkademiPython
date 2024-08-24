180f5db33127e1c8e749f187c5d930dcd18ff34d
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-08-24 14:23:35 +0700

    revisi python 1

--- Basic Python/collection_dict.py
+++ Basic Python/collection_dict.py
@@ -1,20 +1,20 @@
-user = dict()
-list_user = []
-user["nama"] = input("masukan Nama = ")
-user["umur"] = input("masukan Umur = ")
-user["alamat"] = input("masukan Alamat = ")
-user["username"] = input("masukan Username = ")
-user["password"] = input("masukan Password = ")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-list_user.append(user)
-for i in list_user:
-    for key in user:
-        print(key, " = ", user[key])
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# user = dict()
+# list_user = []
+# user["nama"] = input("masukan Nama = ")
+# user["umur"] = input("masukan Umur = ")
+# user["alamat"] = input("masukan Alamat = ")
+# user["username"] = input("masukan Username = ")
+# user["password"] = input("masukan Password = ")
+# print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# list_user.append(user)
+# for i in list_user:
+#     for key in user:
+#         print(key, " = ", user[key])
+# print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 buku_perpus = dict()
 while True:
     print()
-    action = input("Tambah/Hapus ?? ")
+    action = input("Tambah/Hapus/DaftarBuku ?? ")
     if action == "Tambah":
         id_buku = input(" masukan id buku ")
         nama_buku = input(" masukan nama buku ")
@@ -32,5 +32,9 @@ while True:
             print("id buku berhasil dihapus")
         else:
             print("id tidak terdaftar")
+    elif action == "DaftarBuku":
+        print("list id buku dan nama buku")
+        for key in buku_perpus:
+            print(key, " = ", buku_perpus[key])
     else:
         break
--- Basic Python/project1_1.py
+++ Basic Python/project1_1.py
@@ -1,14 +1,17 @@
 input_teks = input(" Tuliskan Teks ")
 input_teks = input_teks.lower()
+# input_teks = input_teks.replace('a', 'i')
 print("Total Karakter ", len(input_teks))
 vokal_dict = dict()
 for i in ['a', 'i', 'u', 'e', 'o']:
     vokal_dict[i] = 0
 total_huruf_vokal = 0
-for i in input_teks:
+for index, i in enumerate(input_teks):
     if i in ['a', 'i', 'u', 'e', 'o']:
         vokal_dict[i] += 1
         total_huruf_vokal += 1
+    if i == 'a':
+        input_teks[index] = 'i'
 print("Total Huruf Vokal = ", total_huruf_vokal)
 for i in vokal_dict:
     print(i, " = ", vokal_dict[i])
