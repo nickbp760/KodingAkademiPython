dec0d65325bf05d121b1eaac964ddaff673dab7e
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2024-09-16 11:57:23 +0700

    Data Plotting & delete C

--- Basic C++/statement.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hallo, tanpa new line");
-    printf("Hallo, dengan new line\n");
-    printf("A string with \"quoted text\" inside of it\n\n");
-    printf("Tabbed\tColumn");
-}
